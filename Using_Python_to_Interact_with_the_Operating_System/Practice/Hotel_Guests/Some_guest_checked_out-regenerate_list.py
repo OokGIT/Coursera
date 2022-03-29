e2b786e4a7ff0bdc9ba93ba3ae997d84fdd36c57
OokGIT <aeroflot.ua@gmail.com>
2022-03-29 11:50:57 +0300

    Some guest checked out

---
+++ Using_Python_to_Interact_with_the_Operating_System/Practice/Hotel_Guests/Some_guest_checked_out-regenerate_list.py
@@ -0,0 +1,11 @@
+checked_out=["Andrea", "Manuel", "Khalid"]
+temp_list=[]
+
+with open("guests.txt", "r") as guests:
+    for g in guests:
+        temp_list.append(g.strip())
+
+with open("guests.txt", "w") as guests:
+    for name in temp_list:
+        if name not in checked_out:
+            guests.write(name + "\n")
