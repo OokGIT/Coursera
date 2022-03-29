b8411dc7a44fd7166e945a8bf35501ec9ec7ee4d
OokGIT <aeroflot.ua@gmail.com>
2022-03-29 12:29:50 +0300

    Check is file exist and get the size of file

---
+++ Using_Python_to_Interact_with_the_Operating_System/Practice/Managing_Files_and_Directories/Is_file_exist__and_filesize.py
@@ -0,0 +1,4 @@
+import os
+print("is_file_exist.py exists -", os.path.exists("Is_file_exist__and_filesize.py"))
+print("userlist.txt exists -", os.path.exists("userlist.txt"))
+print(os.path.getsize("Is_file_exist__and_filesize.py"))
