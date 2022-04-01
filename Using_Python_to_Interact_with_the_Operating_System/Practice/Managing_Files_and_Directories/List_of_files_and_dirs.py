edcbe25249e553a502181aad6f25ba4116196889
OokGIT <aeroflot.ua@gmail.com>
2022-04-01 15:47:13 +0300

    List of dirs and files with determination what is it.

---
+++ Using_Python_to_Interact_with_the_Operating_System/Practice/Managing_Files_and_Directories/List_of_files_and_dirs.py
@@ -0,0 +1,8 @@
+import os
+dir = "Some_Directory"
+for name in os.listdir(dir):
+    fullname = os.path.join(dir, name)
+    if os.path.isdir(fullname):
+        print("{} is a directory".format(fullname))
+    else:
+        print("{} is a file".format(fullname))
